415e6b5d778bce66f579c8a4f2129ea0bd6daaeb
Archis <archismanpaul09@outlook.com>
2025-05-20 20:41:43 +0530

    Fix ig

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 
 function App() {
   return (
     <Routes>
+      <Route path='/' element={<Navigate to='/login' replace />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<SignUp />} />
-
     </Routes>
   )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login <Page></Page></div>
+    <div className="w-full h-[100vh] flex items-center justify-center bg-slate-200">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+        <h1 className="text-2xl font-bold mb-4">Login</h1>
+      </div>
+    </div>
   )
 }
 
