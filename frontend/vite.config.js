7d6db981a3ddb2e62d347e2841af355af4bb09e2
Vikash Gupta <vikash9c35@gmail.com>
2025-06-04 23:38:19 +0530

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/Hooks'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@assets': path.resolve(__dirname, './src/assets')
+    }
+  },
   build: {
     outDir: 'dist',
     sourcemap: false,
